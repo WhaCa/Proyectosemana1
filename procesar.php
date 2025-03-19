0fd3c77e999184339b79a9a7f9b278b944cbec44
WhaCa <antciarup@gmail.com>
2025-03-19 13:49:12 +0100

    Add files via upload
    Sigo con el css

--- procesar.php
+++ procesar.php
@@ -1,7 +1,8 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = htmlspecialchars($_POST["nombre"]);
+    $nombre = htmlspecialchars($_POST["ejercito"]);
 
     echo "<h1>Hola, $nombre</h1>";
 }
 ?>
+
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function cambiarTexto() {
-  document.getElementById("parrafo-especial").textContent =
-    "Texto cambiado por JavaScript";
+function cambiarColor() {
+  document.body.style.backgroundColor =
+    document.body.style.backgroundColor === "firebrick" ? "gold" : "firebrick";
+  document.body.style.color =
+    document.body.style.color === "gold" ? "firebrick" : "gold";
 }
