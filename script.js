0efc55a133777a847a32d7e42c74d0b426416712
WhaCa <antciarup@gmail.com>
2025-03-19 20:07:45 +0100

    Add files via upload
    Añadidos cambios en javascript

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-function cambiarColor() {
-  document.body.style.backgroundColor =
-    document.body.style.backgroundColor === "firebrick" ? "gold" : "firebrick";
-  document.body.style.color =
-    document.body.style.color === "gold" ? "firebrick" : "gold";
+function cambiarTexto() {
+  document.getElementById("parrafo-especial").textContent =
+    "Texto cambiado por JavaScript";
 }
